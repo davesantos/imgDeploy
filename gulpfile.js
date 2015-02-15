5bca8b1f40076ae63f1af130ed9004190d393a62
Dave Santos <info@davesantos.com>
2015-02-14 23:50:45 -0800

    Added svg support, changed variables

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,51 @@ var gulp = require('gulp'),
     newer = require('gulp-newer'),
     rename = require('gulp-rename');
 
-var imgSrc = 'photos/*.{jpg,jpeg,png}',
-    imgDest = 'deploy',
-    imgWidth = 1080;
+var svgmin = require('gulp-svgmin'),
+    svgstore = require('gulp-svgstore');
 
+var img = {
+  src: 'photos/*.{jpg,jpeg,png}',
+  dest: 'deploy',
+  width: 1080
+};
+
+var svg = {
+  src: 'svg/*.svg',
+  dest: 'deploy'
+}
 
 gulp.task('clean', function(cb) {
-  del([imgDest], cb);
+  del([img.dest], cb);
 });
 
 gulp.task('optimize', function(){
-  gulp.src(imgSrc)
-    .pipe(newer(imgDest))
+  gulp.src(img.src)
+    .pipe(newer(img.dest))
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest(imgDest));
+    .pipe(gulp.dest(img.dest));
 });
 
 
 gulp.task('resize', function () {
-  gulp.src(imgSrc)
-    .pipe(newer(imgDest))
+  gulp.src(img.src)
+    .pipe(newer(img.dest))
       .pipe(imageResize({
-        width : imgWidth,
+        width : img.width,
         crop : false,
         upscale : false
       }))
       .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
       .pipe(rename({
-        suffix: "_" + imgWidth.toString()
+        suffix: "_" + img.width.toString()
       }))
-      .pipe(gulp.dest(imgDest));
+      .pipe(gulp.dest(img.dest));
+});
+
+gulp.task('svg', function() {
+  return gulp.src(svg.src)
+    .pipe(svgmin())
+    .pipe(gulp.dest(svg.dest));
 });
 
 gulp.task('default', ['optimize']);
